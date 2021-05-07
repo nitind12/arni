5901d95001f07242fd52d514c67bc7dfb8fce6ee
Dr. Naveen Tewari <navtewari@gmail.com>
2021-05-07 22:32:36 +0530

    up
    up

--- application/views/index.php
+++ application/views/index.php
@@ -18,7 +18,7 @@
     }
 </style>
 <?php
-    
+    /*
     $slider_ = array(
         'bg1.jpg'=>'Arni University',
         'bg2.jpg'=>'Arni University',
@@ -29,7 +29,7 @@
         'bg7.jpg'=>'Arni University',
         'bg8.jpg'=>'Arni University'
     );
-
+*/
     
 ?>
 <!--script>
@@ -50,63 +50,18 @@
                         <?php $this->load->view('templates/menu'); ?>
                     </header>
 
-                        <!-- AGI Video
+                        <!-- AGI Video-->
                         <header style="overflow: hidden; position: relative; height: auto">
                             <div class="video-test"style="position: sticky; overflow: hidden; min-height: 25%; height: 100%">
                               <video preload="auto" style="width:100%; height:auto;" autoplay="autoplay" loop="loop" muted="">
-                                <source src="<?php echo base_url('assets/ai_video.mp4');?>" type="video/mp4">
+                                <source src="<?php echo base_url('assets/arniVideo.mp4');?>" type="video/mp4">
                                  <source src="/images/video/ai_video.ogg" type="video/ogg">
                               </video>
                             </div>
                             <div style="position: absolute; z-index: 999999"></div>
                         </header>
-                        AGI Video -->
-                        <div id="x-corp-carousel" class="carousel slide hero-slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#x-corp-carousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="1"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="2"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="3"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="4"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="5"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="6"></li>
-                            <li data-target="#x-corp-carousel" data-slide-to="7"></li>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <?php $loop1 = 0; $sliderno = mt_rand(0,count($slider_)-1);?>
-                            <?php foreach($slider_ as $img=>$item){?>
-                            <div class="item<?php if($loop1 == $sliderno) echo ' active';?>">
-                                <img src="<?php echo base_url('assets/img/slider/'.$img); ?>?version=2.2" alt="<?php echo $item; ?>">
-                                <!--Slide Image-->
-
-                                <div class="container">
-                                    <div class="carousel-caption">
-                                        <small class="animated fadeIn">CALL US TODAY (Toll Free)</small>
-                                        <div class="phone animated lightSpeedIn">18008918962</div>
-                                        <h1 class="animated lightSpeedIn hidden-xs" style="font-size:40px!important;"><?php echo $item; ?></h1>                                            
-                                    </div>
-                                    <!--.carousel-caption-->
-                                </div>
-                                <!--.container-->
-                            </div>
-                            <!--.item-->
-                            <?php $loop1++;?>
-                            <?php } ?>
-                        </div>
-                        <!--.carousel-inner-->
-
-                        <!-- Controls -->
-                        <a class="left carousel-control" href="#x-corp-carousel" role="button" data-slide="prev">
-                            <i class="fa fa-angle-left" aria-hidden="true"></i>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#x-corp-carousel" role="button" data-slide="next">
-                            <i class="fa fa-angle-right" aria-hidden="true"></i>
-                            <span class="sr-only">Next</span>
-                        </a>
+                        <!--AGI Video -->
+                        
                     </div>
                     <!-- #x-corp-carousel-->
                     <div style="clear: both;"><br></div>
