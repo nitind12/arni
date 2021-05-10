16a19bf57a6bb6156a006f1cc8f65a8539ac9b8a
Dr. Naveen Tewari <navtewari@gmail.com>
2021-05-10 23:51:42 +0530

    up
    up

--- application/views/academics/faculty/art_and_humanities.php
+++ application/views/academics/faculty/art_and_humanities.php
@@ -1,37 +1,133 @@
+<style>
+    .advantage{
+        font-size:40px;
+        color:#010066;
+    }
+</style>
 <section class="trusted-client-wrapper">
-                                
-                                    <div class="col-md-8">
+                                    <div class="col-md-7">
                                         <h6><?php echo $breadcrumb; ?></h6>
                                         <h2 class="section-title wow fadeInDown" ><?php echo $title;?></h2>
                                         <div class="col-md-12" style="border: #fff solid 0px">
-                                                                               
-                                            <p>Text here. </p>
-                                            
-                                        
+                                            <p>Arni School of Arts & Humanties Humanities focuses on all aspects related to culture and creative life including most disciplines outside exact Sciences.</p>
+                                            <p><i>It sharpens your quantitative and analytical skills, there are many disciplines within the field of engineering, with potentially very different career paths.</i></p>                                        
                                         </div><!-- /.col-md-4 -->                               
                                     </div>
-                                    <div class="col-md-4">
-                                        <h2 class="section-title wow fadeInDown" >Arni Edge</h2>
+                                    <div class="col-md-5">
                                         <div class="col-md-12" style="border: #fff solid 0px">
                                             <ul class="list-group">
-                                                <li class="list-group-item" style="padding:5px!important;">200+ Expert Instructors</li>
-                                                <li class="list-group-item" style="padding:5px!important;">State of Art Infrastructure</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Ultra-modern class rooms</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Elegant and well stocked library</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Wi-Fi Campus</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Multi-purpose Gyms</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Modern canteen</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Well maintained hostels</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Industry 4.0 curriculum</li>
-                                                <li class="list-group-item" style="padding:5px!important;">Campus placement facility</li>
+                                                <li class="list-group-item list-group-item-success" style="padding:5px!important;">Top Private Management College in Himachal Pradesh</li>
+                                                <li class="list-group-item list-group-item-danger" style="padding:5px!important;">AICTE Approved & Industry 4.0 Ready Program</li>
+                                                <li class="list-group-item list-group-item-success" style="padding:5px!important;">2-Time Notified By UGC</li>
+                                                <li class="list-group-item list-group-item-danger" style="padding:5px!important;">Excellent Placement Record</li>
+                                                <li class="list-group-item list-group-item-success" style="padding:5px!important;">Vibrant Campus Life</li>
+                                                <li class="list-group-item list-group-item-danger" style="padding:5px!important;">Nurturing Socially Responsible Students.</li>
+                                                <li class="list-group-item list-group-item-success" style="padding:5px!important;">Opportunity to Pursue Tech Management After 12th</li>
                                             </ul>                                    
                                         </div><!-- /.col-md-4 -->                               
                                     </div>
+                                
+                                <div class="col-md-12" style="margin-top:2em;margin-bottom:3em;background:#ffeaa1; padding:2em 0px;border-radius:10px;">
+                                    <div class="col-md-12" style="border: #fff solid 0px">
+                                        <div class="col-sm-3 text-center experiance-tab-content wow fadeInRight">
+                                            <h2 class="advantage">10+</h2>
+                                            <p style="text-align:center">Courses</p>
+                                        </div><!--/.col-sm-2 -->
+                                        <div class="col-sm-3 text-center experiance-tab-content wow fadeInRight">
+                                            <h2 class="advantage">20+</h2>
+                                            <p  style="text-align: center">Faculty</p>
+                                        </div><!--/.col-sm-6 -->
+                                        <div class="col-sm-3 text-center experiance-tab-content wow fadeInLeft">
+                                            <h2 class="advantage"><?php echo "4+";?></h2>
+                                            <p style="text-align:center">Departmets</p>
+                                        </div><!--/.col-sm-2 -->
+                                        <div class="col-sm-3 text-center experiance-tab-content wow fadeInLeft">
+                                            <h2 class="advantage"><?php echo "20+";?></h2>
+                                            <p style="text-align:center">Events</p>
+                                        </div><!--/.col-sm-2 -->
+                                    </div><!-- /.col-md-4 -->  
+                                </div>
 
-                                    <div class="col-md-12" style="clear: both"></div>
-                                    
-                                    <div class="col-md-12" style="clear: both; padding: 10px 0px"></div>
-                               
+                                <h2 class="section-title-dark wow fadeInDown" align="center">Courses</h2>
+                                <div class="panel-group col-md-6" id="accordion">
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                                            English</a>
+                                           </h4>
+                                        </div>
+                                        <div id="collapse1" class="panel-collapse collapse in">
+                                            <ul class = "list-group">
+                                                <li class = "list-group-item">MA(Eng)</li>
+                                                <li class = "list-group-item">M.Phil English</li>
+                                                <li class = "list-group-item">Ph.D English</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                                           Education</a>
+                                        </h4>
+                                        </div>
+                                        <div id="collapse2" class="panel-collapse collapse in">
+                                            <ul class = "list-group">
+                                                <li class = "list-group-item">MA (Economics)</li>
+                                                <li class = "list-group-item">MA (Education)</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+                                            Fashion Design</a>
+                                        </h4>
+                                        </div>
+                                        <div id="collapse5" class="panel-collapse collapse in">
+                                            <ul class = "list-group">
+                                                <li class = "list-group-item">B.Design (Fashion)</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel-group col-md-6" id="accordion">
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                                            Journalism & Mass Communication</a>
+                                        </h4>
+                                        </div>
+                                        <div id="collapse3" class="panel-collapse collapse in">
+                                            <ul class = "list-group">
+                                                <li class = "list-group-item">BA</li>
+                                                <li class = "list-group-item">BA (Journalism and Mass Communication)</li>
+                                                <li class = "list-group-item">MA (Journalism & Mass Communication)</li>
+                                                <li class = "list-group-item">Ph.D Mass Communication</li>
+                                                <li class = "list-group-item">Ph.D General Arts</li>
+                                                <li class = "list-group-item">Ph.D Economics</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+                                            Psychology</a>
+                                        </h4>
+                                        </div>
+                                        <div id="collapse5" class="panel-collapse collapse in">
+                                            <ul class = "list-group">
+                                                <li class = "list-group-item">MA (Psychology)</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <div style="clear:both"></div>
-                                    
                             </section>
+                            
