db382eeb603aa48dfab895e460223733b504f3b6
Dr. Naveen Tewari <navtewari@gmail.com>
2021-05-12 22:18:14 +0530

    up
    up

--- application/controllers/Arni.php
+++ application/controllers/Arni.php
@@ -184,6 +184,8 @@ function contact() {
     $data_['meta'] = $this->metainfo_for_web('home', 'home');
     $data_['menu_active'] =8;
     $data_['menu_all'] = $this->my_menu->site_menu();
+    $data_['titleMain'] = "Contact Us";
+    $data_['breadcrumb'] = 'Contact us';
    
     $this->load->view('templates/header', $data_);
     $this->load->view('contact/index', $data_);
