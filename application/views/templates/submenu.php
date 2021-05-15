980425018f0497d448cf18a8db5d9b211c21d58d
Dr. Naveen Tewari <navtewari@gmail.com>
2021-05-15 11:08:16 +0530

    u
    p

--- application/views/templates/submenu.php
+++ application/views/templates/submenu.php
@@ -1,6 +1,6 @@
 <div class="topnav" id="myTopnav">
 <?php foreach ($submenu as $item => $value) { ?>
-  <a href="<?php echo site_url($value[0]);?>" <?php if($subpage == $value[1]){ echo 'class="active"'; } ?>><?php echo $item; ?></a>
+  <a href="<?php echo site_url($value[0]);?>" <?php if($inner_page == $value[1]){ echo 'class="active"'; } ?>><?php echo $item; ?></a>
 <?php } ?>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
